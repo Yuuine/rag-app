2ae5518916f3907500222bd71f5495f006a5157c
yunqing <jzhang0703@163.com>
2025-12-23 23:55:19 +0800

    feat(ui): 添加静态页面

--- src/main/java/yuuine/ragapp/config/ServiceEndpointProperties.java
+++ src/main/java/yuuine/ragapp/config/ServiceEndpointProperties.java
@@ -3,7 +3,6 @@ package yuuine.ragapp.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
 @ConfigurationProperties(prefix = "services")
